fed3db919599968d512293b3c16b8fc69ba895a8
王俊 <1871015627@qq.com>
2019-01-21 21:31:07 +0800

    close debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       debugShowCheckedModeBanner: false,
       title: '海马代购管理',
       theme: ThemeData(
         primarySwatch: Colors.blue,
